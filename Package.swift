3c62288212fe561e224efc0d811bb8c8c9947cd3
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2024-08-16 12:21:25 +1000

    Fixed SPM

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Package.swift",
+    name: "SliderView",
     platforms: [
         .iOS(.v14),
         .tvOS(.v14),
@@ -13,21 +13,21 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Package.swift",
-            targets: ["Package.swift"]),
+            name: "SliderView",
+            targets: ["SliderView"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Package.swift",
-            dependencies: []),
+            name: "SliderView",
+            dependencies: [],
+            path: "Sources/SliderView"
+        ),
         .testTarget(
-            name: "Package.swiftTests",
-            dependencies: ["Package.swift"]),
+            name: "SliderViewTests",
+            dependencies: ["SliderView"],
+            path: "Tests"
+        ),
     ]
 )
